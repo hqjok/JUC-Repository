a6d3609619e538bb6d9f8f794b002785e64b9ec0
huangqj <huangqj@eastcom-sw.com>
2022-05-09 22:12:31 +0800

    feat: commit.

---
+++ src/com/hqj/JUC/Test.java
@@ -0,0 +1,11 @@
+package com.hqj.JUC;
+
+/**
+ * @Author huangqj
+ * @Create 2022-05-09 22:11
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试合并分支");
+    }
+}
